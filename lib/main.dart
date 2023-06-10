750dbc73d8fa1f7cde0eb37612bd753dfd0f279e
thek1d <oezguer.guemueslue@gmail.com>
2023-06-11 01:16:05 +0200

    replicated layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const LayoutChallenge(),
+  );
+}
+
+class LayoutChallenge extends StatelessWidget {
+  const LayoutChallenge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: const Color.fromARGB(255, 111, 184, 22),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
